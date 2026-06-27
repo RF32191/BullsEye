52ff647694fc01a6b17aae1832a48fbeca26a1f2
Ryan <Fermoselle>
2026-06-27 14:56:35 -0700

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Ryan Joshua Fermoselle on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
